764d9db01af9e417226c8f8e6ece3c9b65e48e75
oyr1234 <106671562+oyr1234@users.noreply.github.com>
2026-03-03 12:13:30 +0100

    mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -6,10 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Backend is running 🚀');
-});
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB')
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.log(err));
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log("Server running on port 3000");
 });
